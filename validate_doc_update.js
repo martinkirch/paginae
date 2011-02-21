1f69c53149731c9a2924375e7d393a8f4dac06cd
Martin Kirchgessner <martin.kirch@gmail.com>
2011-02-22 00:10:32 +0100

    Adding the bookmarklet with its tester

--- validate_doc_update.js
+++ validate_doc_update.js
@@ -8,18 +8,27 @@ function (newDoc, existingDoc, userCtx) {
 		}
 	}
 	
+	function constant(field) {
+		if (existingDoc && newDoc[field] != existingDoc[field]) {
+			throw({forbidden : "Document's \"" + field + "\" field can't be changed."});
+		}
+	}
+	
 	// deletions
 	if (newDoc._deleted) {
 		return;
 	}
 	
-	if (existingDoc && newDoc.type != existingDoc.type) {
-		throw({forbidden : "Document's \"type\" field can't be changed."});
-	}
+	constant('type');
 	
 	switch (newDoc.type) {
 		case "link" :
 			validate(newDoc.created_at, "Creation timestamp is not set.");
+			constant('created_at');
+			validate(newDoc.title, "Link title is not set.");
+			constant('title');
+			validate(newDoc.url, "Creation timestamp is not set.");
+			constant('url');
 			break;
 		default :
 			throw({forbidden : "Invalid \"type\" field."});
