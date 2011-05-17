4f75af2f017c53fb8ff5fed1ee81f9599f369bc0
Marty <martin.kirch@gmail.com>
2011-05-17 14:59:51 +0200

    Using encodeURIComponent instead of window.btoa to encode page's title and URL in the bookmarklet (btoa is not Unicode-safe)

--- evently/bookmarklet/_init/data.js
+++ evently/bookmarklet/_init/data.js
@@ -3,6 +3,6 @@ function () {
 	var currentHostURL = href.substring( 0, href.indexOf('/', 8) );
 	
 	return {
-		href : "javascript:(function(){var d=document,z=d.createElement('ifr'+'ame'),b=d.body;try{if(!b)throw(0);z.setAttribute('src','" + currentHostURL + "/paginae/_design/paginae/saving_iframe.html?url='+window.btoa(window.location.href)+'&title='+window.btoa(document.title));b.appendChild(z);s=z.style;s.position='fixed';s.top=s.left=25;s.width='7em';s.height='3em';setTimeout(function(){b.removeChild(z)},3000);}catch(e){alert('Please%20wait%20until%20the%20page%20has%20loaded.');}})();void(0);"
+		href : "javascript:(function(){var d=document,z=d.createElement('ifr'+'ame'),b=d.body;if(!b)throw(0);z.setAttribute('src','" + currentHostURL + "/paginae/_design/paginae/saving_iframe.html?url='+encodeURIComponent(window.location.href)+'&title='+encodeURIComponent(document.title));b.appendChild(z);s=z.style;s.position='fixed';s.top=s.left=25;s.width='7em';s.height='3em';setTimeout(function(){b.removeChild(z)},3000);})();void(0);"
 	}
 }
